d61d8db7251b1e5f496fc826e05bf47469d2d706
Alex Berliner <alexberliner@gmail.com>
2023-04-24 17:43:53 -0700

    idk haha

--- ScreenReaderDemo/src/bin/wow.rs
+++ ScreenReaderDemo/src/bin/wow.rs
@@ -1,8 +1,10 @@
 use tokio::sync::mpsc::{channel, error};
 use wow_serdes::*;
+use tts::*;
 
 #[tokio::main]
 async fn main() {
+    let mut tts = Tts::default().unwrap();
     let (tx, mut rx) = channel(100);
     let mut handles = vec![];
     let h = tokio::spawn(async move {
@@ -15,9 +17,18 @@ async fn main() {
         loop {
             match rx.try_recv() {
                 Ok(v) => {
-                    let jstring = v.to_string();
-                    eprintln!("payload #{}", ctr);
+                    let jstring = &v.to_string();
+                    let qtts = match v["u"]["qtts"]["questDescription"].as_str() {
+                        Some(v) => v,
+                        None => {"".into()},
+                    };
+                    // eprintln!("payload #{}", ctr);
                     println!("{}",jstring);
+                    // if qtts.len() > 0 {
+                    //     println!("{}", qtts);
+                    // }
+                    // tts.speak(qtts, false);
+                    // println!("{:?}", qtts);
                     ctr += 1;
                 },
                 Err(e) => {
